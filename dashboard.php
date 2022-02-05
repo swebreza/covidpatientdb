5f721083c68c60b538c8bfabe836e5a4534842ed
MD Suweb Reza <45898572+swebreza@users.noreply.github.com>
2022-02-05 11:06:02 +0530

    Updated Counter

--- dashboard.php
+++ dashboard.php
@@ -6,7 +6,6 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
   <title>Untitled</title>
 
-  <?php include './assets/scripts/scripts.html'; ?>
 
 
 
@@ -17,34 +16,8 @@
   <?php include './assets/base/nav.html'; ?>
   <!-- End: Navigation Clean -->
   <!-- Start: Incrementing Animation -->
-  <section data-aos="fade-up" data-aos-duration="400" data-aos-once="true" class="font-dark">
-    <div class="container">
-      <h3 data-aos="fade-up" data-aos-duration="400" data-aos-delay="200" data-aos-once="true" class="text-center all-capital p-4" style="font-size: 30px;font-weight: 300;line-height: 38px;letter-spacing: 2px;">
-        Current Status</h3>
-      <div class="row row-cols-1 row-cols-sm-1 row-cols-md-2 row-cols-lg-4 row-cols-xl-4 row-cols-xxl-4" data-aos="fade-up" data-aos-duration="800" data-aos-delay="150" data-aos-once="true" style="padding-top: 20px;padding-bottom: 40px;">
-        <div class="col">
-          <div><span class="Justify-Flex-Center purecounter counter-font count plus" data-purecounter-start="0" data-purecounter-end="20" data-purecounter-duration="2">20</span>
-            <p class="text-center text-uppercase">Tested Count<br></p>
-          </div>
-        </div>
-        <div class="col">
-          <div><span class="Justify-Flex-Center purecounter counter-font plus" data-purecounter-start="0" data-purecounter-end="450" data-purecounter-duration="1">450</span>
-            <p class="text-center text-uppercase">Positive Count<br></p>
-          </div>
-        </div>
-        <div class="col">
-          <div><span class="Justify-Flex-Center purecounter counter-font plus-k" data-purecounter-start="0" data-purecounter-end="23" data-purecounter-duration="1">23</span>
-            <p class="text-center text-uppercase">Dead Count<br></p>
-          </div>
-        </div>
-        <div class="col">
-          <div><span class="Justify-Flex-Center purecounter counter-font plus" data-purecounter-start="0" data-purecounter-end="275" data-purecounter-duration="1">275</span>
-            <p class="text-center text-uppercase">Recovered Count<br></p>
-          </div>
-        </div>
-      </div>
-    </div>
-  </section><!-- End: Incrementing Animation -->
+  <?php include './count.php'; ?>
+  <!-- End: Incrementing Animation -->
   <div>
     <button class="btn btn-success" style="margin-left: 5px;">
       <a href='./register.php' style="color: aliceblue;">Add Patient</a>
@@ -147,7 +120,7 @@
       </div>
     </div>
   </footer><!-- End: Footer Clean -->
-  <script src="./assets/js/script.min.js?h=eb1f68cfb4a8e6bfebef5f9dbf486544"></script>
+
 </body>
 
 </html>
--- index.php
+++ index.php
@@ -5,7 +5,7 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
   <title>Index</title>
-  <?php include './assets/scripts/scripts.html'; ?>
+
 </head>
 
 <body>
@@ -25,39 +25,11 @@
       </div>
     </div>
   </nav><!-- End: Navigation Clean -->
-  <!-- Start: Incrementing Animation -->
-  <section data-aos="fade-up" data-aos-duration="400" data-aos-once="true" class="font-dark">
-    <div class="container">
-      <h3 data-aos="fade-up" data-aos-duration="400" data-aos-delay="200" data-aos-once="true" class="text-center all-capital p-4" style="font-size: 30px;font-weight: 300;line-height: 38px;letter-spacing: 2px;">
-        Current Status</h3>
-      <div class="row row-cols-1 row-cols-sm-1 row-cols-md-2 row-cols-lg-4 row-cols-xl-4 row-cols-xxl-4" data-aos="fade-up" data-aos-duration="800" data-aos-delay="150" data-aos-once="true" style="padding-top: 20px;padding-bottom: 40px;">
-        <div class="col">
-          <div><span class="Justify-Flex-Center purecounter counter-font count plus" data-purecounter-start="0" data-purecounter-end="20" data-purecounter-duration="2">20</span>
-            <p class="text-center text-uppercase">Tested Count<br></p>
-          </div>
-        </div>
-        <div class="col">
-          <div><span class="Justify-Flex-Center purecounter counter-font plus" data-purecounter-start="0" data-purecounter-end="450" data-purecounter-duration="1">450</span>
-            <p class="text-center text-uppercase">Positive Count<br></p>
-          </div>
-        </div>
-        <div class="col">
-          <div><span class="Justify-Flex-Center purecounter counter-font plus-k" data-purecounter-start="0" data-purecounter-end="23" data-purecounter-duration="1">23</span>
-            <p class="text-center text-uppercase">Dead Count<br></p>
-          </div>
-        </div>
-        <div class="col">
-          <div><span class="Justify-Flex-Center purecounter counter-font plus" data-purecounter-start="0" data-purecounter-end="275" data-purecounter-duration="1">275</span>
-            <p class="text-center text-uppercase">Recovered Count<br></p>
-          </div>
-        </div>
-      </div>
-    </div>
-  </section>
+  <?php include './count.php'; ?>
   <!-- End: Incrementing Animation -->
 
 
-  <script src="./assets/js/script.min.js?h=eb1f68cfb4a8e6bfebef5f9dbf486544"></script>
+
 
 </body>
 
